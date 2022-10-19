6e9f44640178b6912ad65bff0e13eca354241b9d
Fernando Gallego Villanueva <a22fernandogv@ateis.local>
2022-10-19 17:12:31 +0200

    segundo commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         Programador programador1 = new Programador();
+        Programador programador2 = new Programador();
 
         /* Input */
         System.out.println("Escriba el nombre del programador :");
@@ -16,7 +17,15 @@ public class Main {
         System.out.println("¿En qué lenguaje programa? :");
         programador1.lenguaje = sc.nextLine();
 
+        System.out.println("Escriba el nombre del programador :");
+        String lineaNombre2 = sc.nextLine(); // Todo: revisar lectura de ints como chars.
+        programador2.nombre = lineaNombre2;
+
+        System.out.println("¿En qué lenguaje programa? :");
+        programador2.lenguaje = sc.nextLine();
+
         /* Output */
         System.out.println(programador1);
+        System.out.println(programador2);
     }
 }
